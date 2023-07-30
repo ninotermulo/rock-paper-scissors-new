4c49ccf9a5cccdc2ea78bb15e685cca83cff4c64
ninotermulo <ninotermulo01@gmail.com>
2023-07-30 20:21:32 +0800

    Remove instructive comments at last line of code

--- script.js
+++ script.js
@@ -19,22 +19,7 @@ startButton.addEventListener("click", (e) => {
     iconsContainer.appendChild(rockIcon);
     iconsContainer.appendChild(paperIcon);
     iconsContainer.appendChild(scissorIcon);
-    console.log(iconsContainer);
 
     const rpsIcon = document.querySelector(".rps-icon");
     document.querySelector(".main-container").replaceChild(iconsContainer, rpsIcon);
 });
-
-/*
-create selection div wrapper
-    - add id attribute to div
-add rock icon image inside div
-add paper icon image inside div
-add scissor icon image inside div
-    - add class attribute and value for hover effect
-replace rps-icon.png img element with div wrapper with icons
-create p element
-create  text node for selection statement
-append text node inside p element
-replace p element 'start' with p element selection
-*/
