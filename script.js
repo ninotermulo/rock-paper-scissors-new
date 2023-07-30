4c34d305fad3dbfbcdc0619de127eda00d25ac18
ninotermulo <ninotermulo01@gmail.com>
2023-07-30 19:39:41 +0800

    Change class name to 'icon'

--- script.js
+++ script.js
@@ -6,15 +6,15 @@ startButton.addEventListener("click", (e) => {
     
     const rockIcon = document.createElement('img');
     rockIcon.setAttribute('src', "/images/icons/rock-icon.png");
-    rockIcon.setAttribute('class', "choice-hover rps-icon");
+    rockIcon.setAttribute('class', "icons");
     
     const paperIcon = document.createElement('img');
     paperIcon.setAttribute('src', "/images/icons/paper-icon.png");
-    paperIcon.setAttribute('class', "choice-hover rps-icon");
+    paperIcon.setAttribute('class', "icons");
     
     const scissorIcon = document.createElement('img');
     scissorIcon.setAttribute('src', "/images/icons/scissor-icon.png");
-    scissorIcon.setAttribute('class', "choice-hover rps-icon");
+    scissorIcon.setAttribute('class', "icons");
     
     iconsContainer.appendChild(rockIcon);
     iconsContainer.appendChild(paperIcon);
