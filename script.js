4e56c859f09fc752924a039dedeaee56ed22a4f5
ninotermulo <ninotermulo01@gmail.com>
2023-07-30 19:24:36 +0800

    Replace const with let on startButton variable
    Remove keydown event listener

--- script.js
+++ script.js
@@ -1,5 +1,40 @@
-let startButton = document.querySelector(".start");
+const startButton = document.querySelector(".start");
 
-startButton.addEventListener("keydown", (e) => {
-    console.log(e.target);
+startButton.addEventListener("click", (e) => {
+    const iconsContainer = document.createElement('div');
+    iconsContainer.setAttribute('id', 'choice-cont');
+    
+    const rockIcon = document.createElement('img');
+    rockIcon.setAttribute('src', "/images/icons/rock-icon.png");
+    rockIcon.setAttribute('class', "choice-hover rps-icon");
+    
+    const paperIcon = document.createElement('img');
+    paperIcon.setAttribute('src', "/images/icons/paper-icon.png");
+    paperIcon.setAttribute('class', "choice-hover rps-icon");
+    
+    const scissorIcon = document.createElement('img');
+    scissorIcon.setAttribute('src', "/images/icons/scissor-icon.png");
+    scissorIcon.setAttribute('class', "choice-hover rps-icon");
+    
+    iconsContainer.appendChild(rockIcon);
+    iconsContainer.appendChild(paperIcon);
+    iconsContainer.appendChild(scissorIcon);
+    console.log(iconsContainer);
+
+    const rpsIcon = document.querySelector(".rps-icon");
+    document.querySelector(".main-container").replaceChild(iconsContainer, rpsIcon);
 });
+
+/*
+create selection div wrapper
+    - add id attribute to div
+add rock icon image inside div
+add paper icon image inside div
+add scissor icon image inside div
+    - add class attribute and value for hover effect
+replace rps-icon.png img element with div wrapper with icons
+create p element
+create  text node for selection statement
+append text node inside p element
+replace p element 'start' with p element selection
+*/
