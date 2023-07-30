1eb7c5a35df01b91872594bbd688b8c49098787f
ninotermulo <ninotermulo01@gmail.com>
2023-07-30 21:19:53 +0800

    Remove variable declartion iconsContainer

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
 const startButton = document.querySelector(".start");
 
 startButton.addEventListener("click", (e) => {
-    const iconsContainer = document.createElement('div');
-    iconsContainer.setAttribute('id', 'choice-cont');
-    console.log(e);
-    
+
     const rockIcon = document.createElement('img');
     rockIcon.setAttribute('src', "/images/icons/rock-icon.png");
     rockIcon.setAttribute('class', "icons");
@@ -16,11 +13,13 @@ startButton.addEventListener("click", (e) => {
     const scissorIcon = document.createElement('img');
     scissorIcon.setAttribute('src', "/images/icons/scissor-icon.png");
     scissorIcon.setAttribute('class', "icons");
-    
-    iconsContainer.appendChild(rockIcon);
-    iconsContainer.appendChild(paperIcon);
-    iconsContainer.appendChild(scissorIcon);
 
-    const rpsIcon = document.querySelector(".rps-icon");
-    document.querySelector(".icon-container").replaceChild(iconsContainer, rpsIcon);
+    const iconsContainer = document.querySelector(".icon-container");
+    const iconNodesContainer = document.createElement('div');
+    document.querySelector(".rps-icon").remove();
+    
+    iconNodesContainer.appendChild(rockIcon);
+    iconNodesContainer.appendChild(paperIcon);
+    iconNodesContainer.appendChild(scissorIcon);
+    iconsContainer.appendChild(iconNodesContainer);
 });
