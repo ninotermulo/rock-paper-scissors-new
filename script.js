b2549caa0ba375afb1eba20ed3f7c5004fd2a7a7
ninotermulo <ninotermulo01@gmail.com>
2023-08-01 23:21:23 +0800

    Add function that gets user and computr choice
    Gets user choice through click event and computer's choice randomly

--- script.js
+++ script.js
@@ -2,11 +2,15 @@ const startButton = document.querySelector("#p-container > p");
 startButton.addEventListener("click", switchSelectPage);
 
 function switchSelectPage() {
-    document.querySelector(".rps").remove();   
+    const imgCont = document.querySelector("#icon-container");
+    if (imgCont.querySelector('.rps') !== null) {
+        document.querySelector('.rps').remove();
+    }
+
     insertIcons();
     let iconElements = document.querySelectorAll('img.icons');
     iconElements.forEach((item) => {
-    item.addEventListener('click', iconClick)});
+    item.addEventListener('click', getUserChoice)});
     changePar('#p-container > p', 'CHOOSE YOUR WEAPON');
 }
 
@@ -48,6 +52,19 @@ function changePar(element, paragraph) {
     document.querySelector(element).classList.remove("start-hover");
 }
 
-function iconClick(e) {
-    console.log(e);    
+function getUserChoice(e) {
+    return e.target.id;    
+}
+
+// Get computer's input
+function getComputerChoice() {
+    const computerChoice = Math.floor((Math.random() * 3)) + 1;
+    switch(computerChoice) {
+      case 1:
+        return "rock";
+      case 2:
+        return "paper";
+      case 3:
+        return "scissor";
+    }
 }
